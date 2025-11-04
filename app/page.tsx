37440ca12b6ef1f9edcb897f2b743400df2e4303
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-11-04 12:40:05 -0500

    BG-1 chore: removed unused icons

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="min-h-screen p-8 bg-aquablue dark:bg-lightgray">
       <ThemeSwitcher />
-      <div className="mt-8 space-y-4">
+      <div className="mt-8 space-y-4 flex flex-col dark:gap-12 gap-6">
         <div className="bg-aquablue p-4">Words for color to show</div>
         <div className="bg-lightgray p-4">Words for color to show</div>
         <div className="bg-zomp p-4">Words for color to show</div>
