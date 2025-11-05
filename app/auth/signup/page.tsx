3e973741a3aa43444ce35b9464147764e9043188
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-11-04 23:17:05 -0500

    BG-3 fix: fixed global styling background color selector

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,11 @@
+import SignUpForm from "@/components/auth/sign-up-form";
 import ThemeSwitcher from "@/components/theme-switcher";
 
 export default function Page() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <ThemeSwitcher />
+      <SignUpForm />
+    </div>
+  );
 }
--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -2,3 +2,24 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "../ui/card";
+
+export default function SignUpForm() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Title</CardTitle>
+        <CardDescription>Description</CardDescription>
+      </CardHeader>
+      <CardContent>Content</CardContent>
+      <CardFooter>Footer</CardFooter>
+    </Card>
+  );
+}
