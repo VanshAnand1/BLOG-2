87a50e662d2baede28075a7c57b2ca6adf7e2b78
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-11-29 12:00:33 -0500

    BG-4 feature: search bar component

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,10 +1,33 @@
-export default function RootLayout({
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/lib/supabase/server";
+import Link from "next/link";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await (await supabase).auth.getUser();
+
+  let anonymous = false;
+  if (user?.user_metadata?.is_anonymous) {
+    anonymous = true;
+  }
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <div className="w-full bg-red-400 py-3 px-2">
+        <div>
+          <Link href="/">BLOG-2</Link>
+        </div>
+        <div></div>
+        <div>
+          <Button>Hello</Button>
+        </div>
+      </div>
       <body>{children}</body>
     </html>
   );
---
+++ components/navigation-bar/search-bar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push(`/search${searchQuery}`);
+  };
+
+  return (
+    <form onClick={handleSearch}>
+      <div className="flex w-full">
+        <div className="relative flex-1">
+          <span className="pointer-events-none absolute inset-y-0 left-3 flex items-center">
+            <svg
+              className="h-5 w-5 dark:text-black/60 text-white/60"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              aria-hidden="true"
+            >
+              <circle cx="11" cy="11" r="7" />
+              <path d="M20 20l-3.5-3.5" />
+            </svg>
+            <Input
+              type="text"
+              id="search"
+              placeholder="Search..."
+              value={searchQuery}
+              className="w-full h-10 rounded-l-xl bg-neutral-500 text-white placeholder-white/50 pl-10 pr-3 border border-white/10 focus:outline-none focus:ring-2 dark:focus:ring-teagreen focus:border-transparent"
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
+              }}
+            ></Input>
+          </span>
+        </div>
+        <Button
+          type="submit"
+          className="text-white dark:text-black h-10 px-4 rounded-r-xl dark:bg-teagreen/80 font-bold dark:hover:bg-teagreen transition bg-navy/80 hover:bg-navy/60"
+        >
+          Search
+        </Button>
+      </div>
+    </form>
+  );
+}
